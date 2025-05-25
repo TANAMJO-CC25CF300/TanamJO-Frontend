d02e43fc8f988dcd97588c08610d8093e6722a59
nurulalyh <nurulaliyah2303@gmail.com>
2025-05-25 15:07:03 +0800

    feat: add Card1 component

---
+++ src/pages/landingPages/homePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Card1 from "../../components/Card1";
+
+const HomePage = () => {
+    return (
+        <section className="px-4 py-8">
+             
+        </section>
+    )
+}
+
+export default HomePage;
