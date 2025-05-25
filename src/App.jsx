5b3c9ebb94864a497955c98a8ea10594edf6cabb
meyieee <elizabetsiwy@gmail.com>
2025-05-25 15:48:52 +0800

    Adding little button & card 2 components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Card2 from "./Components/Card2";
+import image1 from "./assets/tomato1.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Hello World</h1>
+      <Card2
+        image={image1}
+        type="FERTILIZER"
+        title="Harvest Concepts"
+        description="Farming and animal husbandry and discuss with farmers and scientists."
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
