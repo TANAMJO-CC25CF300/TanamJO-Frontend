f1fdb5a7dccfa326d718627d1081bbaeb97ae1e2
meyieee <elizabetsiwy@gmail.com>
2025-05-25 22:14:16 +0800

    adding cameraCapture & IdentifyPlant

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Card1 from "./components/Card1";
 import image1 from "./assets/tomato1.png";
 import HomePage from "./pages/landingPages/homePage";
+import IdentifyPlant from "./pages/landingPages/IdentifyPlant";
 
 function App() {
   return (
-    // <div>
-    //   <h1>Hello World</h1>
-    //   <Card2
-    //     image={image1}
-    //     type="FERTILIZER"
-    //     title="Harvest Concepts"
-    //     description="Farming and animal husbandry and discuss with farmers and scientists."
-    //   />
-
     <BrowserRouter>
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<HomePage />} />
-      </Routes>
-      <Card1 />
+      </Routes> */}
+      <IdentifyPlant />
     </BrowserRouter>
   );
 }
--- src/pages/landingPages/homePage.jsx
+++ src/pages/landingPages/homePage.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import Card1 from "../../components/Card1";
 
 const HomePage = () => {
-    return (
-        <section className="px-4 py-8">
-             
-        </section>
-    )
-}
+  return <section className="px-4 py-8"></section>;
+};
 
 export default HomePage;
