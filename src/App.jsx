6e13c2db226cddc25fb93001a8ff5e9cdfba4b84
meyieee <elizabetsiwy@gmail.com>
2025-05-25 18:47:14 +0800

    adding header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import Card2 from "./Components/Card2";
-import image1 from "./assets/tomato1.png";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <Card2
-        image={image1}
-        type="FERTILIZER"
-        title="Harvest Concepts"
-        description="Farming and animal husbandry and discuss with farmers and scientists."
-      />
+      <Header />
     </div>
   );
 }
