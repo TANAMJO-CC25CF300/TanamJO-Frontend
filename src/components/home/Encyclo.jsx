4d9892ed406fa98344a03b63841ec8114d3f976b
RISKICODE0104 <riskiary1402@gmail.com>
2025-06-06 00:45:30 +0700

    update encyclo

--- src/components/home/Encyclo.jsx
+++ src/components/home/Encyclo.jsx
@@ -163,39 +163,80 @@ function PlantGuide() {
                         </div>
                     </div>
 
-                    <div className="flex flex-col items-stretch mt-8 md:mt-10 ml-0 md:ml-0 mx-auto relative">
-                        <div className="w-full md:w-full lg:w-full xl:max-w-lg min-h-[280px] md:min-h-[260px] xl:min-h-[320px] rounded-t-3xl md:rounded-3xl overflow-hidden relative">
-                            <img src="src/assets/Banner/banner-1.png" className="absolute inset-0 w-full h-full object-cover" alt="Banner Image" />
-
-                            <div className="absolute inset-0 bg-gradient-to-r from-green-900/30 via-green-800/45 to-transparent pointer-events-none"></div>
-                        </div>
-                        
-                        {/* New card sectionn */}
-                        <div className="w-full md:w-full lg:w-full xl:max-w-lg -mt-18 relative xl:ml-8">
-                            <div className="bg-white rounded-3xl shadow-md p-5 sm:p-5 md:max-w-full lg:max-w-full xl:max-w-md">
-                                <div className="flex flex-col sm:flex-row items-start sm:items-center mb-4 space-y-3 sm:space-y-0">
-                                    <div className="flex items-center">
-                                        <img src={card1} alt="Martin Bailey" className="w-12 h-12 rounded-full mr-4 object-cover" />
+                    <div className="flex flex-col md:flex-row items-stretch mt-8 md:mt-10 ml-0 md:ml-0 mx-auto relative gap-6">
+                        {/* Left Column */}
+                        <div className="flex-1 flex flex-col">
+                            <div className="w-full min-h-[280px] md:min-h-[260px] xl:min-h-[320px] rounded-t-3xl md:rounded-3xl overflow-hidden relative">
+                                <img src="src/assets/Banner/banner-1.png" className="absolute inset-0 w-full h-full object-cover" alt="Banner Image" />
+                                <div className="absolute -top-1 right-[-0.2rem] w-11 h-11 sm:w-12 sm:h-12 md:w-12 md:h-12 bg-white rounded-bl-3xl rounded-tr-3xl z-10 flex items-center justify-center">
+                                    <span className="custom-banner rounded-full w-7 h-7 flex items-center justify-center shadow-sm">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-3 h-3 text-white">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
+                                        </svg>
+                                    </span>
+                                </div>
+                                <div className="absolute inset-0 bg-gradient-to-r from-green-900/30 via-green-800/45 to-transparent pointer-events-none"></div>
+                            </div>
+                            
+                            {/* New card section */}
+                            <div className="w-full -mt-6 md:-mt-8 lg:-mt-10 xl:-mt-15 relative">
+                                <div className="bg-white rounded-3xl shadow-md p-5 sm:p-5">
+                                    <div className="flex flex-col sm:flex-row items-start sm:items-center mb-4 space-y-3 sm:space-y-0">
+                                        <div className="flex items-center">
+                                            <img src={card1} alt="Martin Bailey" className="w-12 h-12 rounded-full mr-4 object-cover" />
+                                            <div>
+                                                <div className="font-semibold font-nunito tracking-wider leading-tight steel-haze">Martin Bailey</div>
+                                                <div className="text-sm/6 font-medium tracking-wider text-gray-500 leading-tight">Supervisor</div>
+                                            </div>
+                                        </div>
+                                        <div className="sm:ml-auto text-sm/6 custome-deskription mt-1.5 sm:mt-0 font-medium leading-normal tracking-wider">9 february, 2024</div>
+                                    </div>
+                                    <div className="space-y-2.5">
+                                        <h3 className="text-xl font-semibold font-nunito tracking-wider leading-tight steel-haze mb-3 sm:line-clamp-none line-clamp-2">What technology is used in vertical farming?</h3>
+                                        <p className="text-sm/6 md:text-sm/6 font-medium leading-normal tracking-wider line-clamp-3 custome-deskription mb-4">
+                                            Growing tomatoes isn't just about planting — it's about knowing what your plants need at every stage. Through our consultation-based approach, we help you choose the right tools, organic inputs, and growing methods tailored specifically for tomatoes.
+                                        </p>
                                         <div>
-                                            <div className="font-semibold text-gray-800">Martin Bailey</div>
-                                            <div className="text-sm text-gray-500">SUPERVISOR</div>
+                                            <a href="#" className="text-sm/6 font-semibold custome-deskription underline">Read More</a>
                                         </div>
                                     </div>
-                                    <div className="sm:ml-auto text-sm text-gray-500 mt-1.5 sm:mt-0">9 february, 2024</div>
                                 </div>
-                                <div className="space-y-2.5">
-                                    <h3 className="text-xl font-semibold font-nunito tracking-wider leading-tight steel-haze mb-3 sm:line-clamp-none line-clamp-2">What technology is used in vertical farming?</h3>
+                            </div>
+                        </div>
 
-                                    <p className="text-sm/6 md:text-sm/6 font-medium leading-normal tracking-wider line-clamp-3 custome-deskription mb-4">
-                                        Growing tomatoes isn't just about planting — it's about knowing what your plants need at every stage. Through our consultation-based approach, we help you choose the right tools, organic inputs, and growing methods tailored specifically for tomatoes.
-                                    </p>
+                        {/* Right Column - Artikel 1 */}
+                        <div className="flex-1 min-h-[48px] p-0 rounded-3xl tj-text-gray font-semibold flex flex-col lg:flex-row items-start gap-4">                                
+                            <div className="relative w-full lg:w-60 xl:w-60 h-40 flex-shrink-0">
+                                <img className="h-full w-full object-cover rounded-3xl" src="src/assets/Banner/banner-1.png" alt="Smart Planting Needs"/>
+                                <div className="absolute -top-1 right-[-0.2rem] w-11 h-11 sm:w-12 sm:h-12 md:w-12 md:h-12 bg-white rounded-bl-3xl rounded-tr-3xl z-10 flex items-center justify-center">
+                                    <span className="custom-banner rounded-full w-7 h-7 flex items-center justify-center shadow-sm">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-3 h-3 text-white">
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25" />
+                                        </svg>
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div className="relative flex flex-col items-start text-left w-full lg:ml-2 ml-1 pt-2 min-h-[190px]">
+                                <div className="w-full lg:max-w-7xl space-y-3 md:space-y-2 pr-2 lg:pr-4 xl:pr-8 2xl:pr-20">
+                                    <h2 className="text-xl md:text-2xl font-bold font-nunito tracking-wider leading-tight steel-haze">
+                                        Smart Watering System
+                                    </h2>
                                     
+                                    <p className="text-sm/6 md:text-sm/6 font-medium leading-normal tracking-wider line-clamp-3 custome-deskription max-w-[35rem] mb-3.5">
+                                        Smart watering systems use sensors and automated controls to optimize water usage for plants. These systems monitor soil moisture, weather conditions, and plant needs to deliver the right amount of water at the right time, promoting healthy growth while conserving resources.
+                                    </p>
+
+                                    <div className="text-sm/6 custome-deskription font-medium leading-normal tracking-wider mb-4">
+                                        15 February, 2024
+                                    </div>
+
                                     <div>
                                         <a href="#" className="text-sm/6 font-semibold custome-deskription underline">Read More</a>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div>  
                     </div>
                 
                 </div>  
