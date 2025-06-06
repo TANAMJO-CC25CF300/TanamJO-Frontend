68b809855285ac73033b7694301d20b986ccca68
EKO <e1304h@gmail.com>
2025-06-06 22:05:22 +0700

    fix left sidebar

---
+++ src/components/dashboard/Left-Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const LeftSidebar = () => {
+  return (
+    <aside style={{ width: "200px", background: "#f0f0f0", height: "100vh", padding: "1rem" }}>
+      <nav>
+        <ul style={{ listStyle: "none", padding: 0 }}>
+          <li><a href="/">Home</a></li>
+          <li><a href="/plant-guide">Plant Guide</a></li>
+          <li><a href="/encyclopedia">Encyclopedia</a></li>
+        </ul>
+      </nav>
+    </aside>
+  );
+};
+
+export default LeftSidebar;
