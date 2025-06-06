a0e3ac038a102fdbe50b79894a9bb187a9bff7b3
EKO <e1304h@gmail.com>
2025-06-06 22:14:56 +0700

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import PlantGuide from './components/home/PlantGuide';
 import Encyclo from './components/home/Encyclo';
+import Dashboard from './components/dashboard/Index';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<PlantGuide/>} />
         <Route path="/plant-guide" element={<PlantGuide />} />
         <Route path="/encyclopedia" element={<Encyclo />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </>
   );
