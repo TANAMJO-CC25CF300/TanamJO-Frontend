fb9b6dee0777b3fedbc4a140c43b718336890cd4
EKO <e1304h@gmail.com>
2025-06-06 23:24:34 +0700

    add/update housplants and encyclo components

---
+++ src/components/home/Housplants.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import HarvestLanding from '../plantlis/HarvestLandingHome';
+
+function PlantGuide() {
+    return (
+
+        <main>            
+            <div>
+                <HarvestLanding title="Our History" breadcrumb={["Home", "Plant Care Guide"]}/>
+            </div>
+
+            <div className="lg:mx-0 xl:mx-10 -mt-6 w-auto rounded-4xl text-center relative z-20 mb-5 shadow-md pattern-background">
+
+                                   
+            </div>
+        </main>
+        
+    );
+}
+
+export default PlantGuide;    
