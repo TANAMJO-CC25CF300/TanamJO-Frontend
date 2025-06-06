1f08c6b9101f0f72154351b3b9772791c18cb1a0
EKO <e1304h@gmail.com>
2025-06-06 19:35:21 +0700

    fix litten button

--- src/components/button/LittenButton.jsx
+++ src/components/button/LittenButton.jsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 
 function LittenButton() {
     return (
-        <main>
-            
+
+        <main>            
             {/* Mobile version */}
             <div className="py-0 text-gray-700">
                 <Link to="/" className="-mx-3 block px-3 py-3.5 text-md/7 font-semibold border border-gray-300 rounded-full text-center">
                     Sign Up
                 </Link>
-            </div>
-        
+            </div>        
         </main>
+
     );
 }
 
