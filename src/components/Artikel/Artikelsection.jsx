8d72af37f98abdb6cc28a0bfe5b4fb40400523de
EKO <e1304h@gmail.com>
2025-06-06 20:16:17 +0700

    fix Artikel section

--- src/components/Artikel/Artikelsection.jsx
+++ src/components/Artikel/Artikelsection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import smartWateringImage from "../../assets/Banner/card-8.png";
+import smartWateringImage from "../../assets/Banner/card-5.png";
 import urbanFarmingImage from "../../assets/Banner/card-1.png";
 import sustainableGardeningImage from "../../assets/Banner/card-2.png";
 
