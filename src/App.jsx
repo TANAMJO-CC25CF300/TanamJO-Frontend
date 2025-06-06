b02a3a8bef620ce8ee02e009a0feba9ce684a872
EKO <e1304h@gmail.com>
2025-06-06 22:12:36 +0700

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,16 @@ import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import PlantGuide from './components/home/PlantGuide';
 import Encyclo from './components/home/Encyclo';
-import HeaderDashboard from './components/dashboard/Header-dashboard';
-import LeftSidebar from './components/dashboard/Left-Sidebar';
-
-// Layout khusus dashboard
-function DashboardLayout({ children }) {
-  return (
-    <>
-      <HeaderDashboard />
-      <div style={{ display: 'flex' }}>
-        <LeftSidebar />
-        <main style={{ flex: 1, padding: '1rem' }}>
-          {children}
-        </main>
-      </div>
-    </>
-  );
-}
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<PlantGuide />} />
-      <Route path="/plant-guide" element={<PlantGuide />} />
-      <Route path="/encyclopedia" element={<Encyclo />} />
-      {/* Route khusus dashboard */}
-      <Route path="/dashboard/*" element={
-        <DashboardLayout>
-          {/* Tambahkan komponen dashboard di sini */}
-          <div>Dashboard Content</div>
-        </DashboardLayout>
-      } />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<PlantGuide/>} />
+        <Route path="/plant-guide" element={<PlantGuide />} />
+        <Route path="/encyclopedia" element={<Encyclo />} />
+      </Routes>
+    </>
   );
 }
 
