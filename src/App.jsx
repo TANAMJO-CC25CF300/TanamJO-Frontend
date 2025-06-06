0f7e44b00a00c2fab4bdab76ed03228165a73c97
EKO <e1304h@gmail.com>
2025-06-06 22:04:47 +0700

    fix header dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import PlantGuide from './components/home/PlantGuide';
 import Encyclo from './components/home/Encyclo';
+import HeaderDashboard from './components/dashboard/Header-dashboard';
+import LeftSidebar from './components/dashboard/Left-Sidebar';
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<PlantGuide/>} />
-        <Route path="/plant-guide" element={<PlantGuide />} />
-        <Route path="/encyclopedia" element={<Encyclo />} />
-      </Routes>
+      <HeaderDashboard />
+      <div style={{ display: 'flex' }}>
+        <LeftSidebar />
+        <main style={{ flex: 1, padding: '1rem' }}>
+          <Routes>
+            <Route path="/" element={<PlantGuide/>} />
+            <Route path="/plant-guide" element={<PlantGuide />} />
+            <Route path="/encyclopedia" element={<Encyclo />} />
+          </Routes>
+        </main>
+      </div>
     </>
   );
 }
---
+++ src/components/dashboard/Header-dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HeaderDashboard = () => {
+  return (
+    <header style={{ padding: "1rem", background: "#e0ffe0", textAlign: "center" }}>
+      <h1>Dashboard Header</h1>
+    </header>
+  );
+};
+
+export default HeaderDashboard;
