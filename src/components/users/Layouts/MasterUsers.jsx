232f25d19c06ae6a1074914e56ae8eac13d77ed1
EKO <e1304h@gmail.com>
2025-06-07 22:55:15 +0700

    Add master users

--- src/components/users/Layouts/MasterUsers.jsx
+++ src/components/users/Layouts/MasterUsers.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { Helmet } from 'react-helmet-async';
-
+import Header from './Header';
+import LeftSidebar from './LeftSidebar';
+import Dashboard from '../Dashboard';
 
 function MasterUsers() {
     return (
         <>
             <Helmet>
-
+                {/* Tempatkan link/script tambahan di sini jika perlu */}
             </Helmet>
+
+            <Header />
+            
+            <div style={{ display: 'flex' }}>
+                <LeftSidebar />
+                <Dashboard />
+            </div>
         </>
     )
 }
