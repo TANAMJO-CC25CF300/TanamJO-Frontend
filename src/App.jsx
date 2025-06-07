846eb6aceae61d98f05523de4314835cd958be00
EKO <e1304h@gmail.com>
2025-06-07 12:26:01 +0700

    fix src app

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import PlantGuide from './components/home/PlantGuide';
 import Encyclo from './components/home/Encyclo';
-import Dashboard from './components/dashboard/Index';
 import Chery_tomato from './components/home/Chery_tomato';
 import Beefsetak_tomato from './components/home/Beefsetak_tomato';
 import Roma_tomato from './components/home/Roma_tomato';
 import Heirloom_tomato from './components/home/Heirloom_tomato';
+import DashboardLayout from './components/dashboard/DashboardLayout/LayoutDashboard';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         <Route path="/" element={<PlantGuide/>} />
         <Route path="/plant-guide" element={<PlantGuide />} />
         <Route path="/encyclopedia" element={<Encyclo />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<DashboardLayout />} />
         <Route path="/Chery_tomato" element={<Chery_tomato />} />
         <Route path="/Beefsetak_tomato" element={<Beefsetak_tomato />} />
         <Route path="/roma_tomato" element={<Roma_tomato />} />
