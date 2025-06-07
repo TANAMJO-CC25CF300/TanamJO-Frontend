14a20bdc8adc5a7a7ba1a74bbc920b34f196f82a
EKO <e1304h@gmail.com>
2025-06-07 15:42:07 +0700

    update sidebar

--- src/components/dashboard/sidebar.jsx
+++ src/components/dashboard/sidebar.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Sidebar = () => {
+function Sidebar() {
+
   return (
 
     <div className="layout-wrapper layout-content-navbar">
