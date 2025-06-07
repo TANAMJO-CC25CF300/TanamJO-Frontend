42da9e2a967da27f324b5ed0db66fbf91253e310
EKO <e1304h@gmail.com>
2025-06-07 09:13:41 +0700

    fix left sidebar

--- src/components/dashboard/Left-Sidebar.jsx
+++ src/components/dashboard/Left-Sidebar.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 
-
 const LeftSidebar = () => {
   return (
-    <aside style={{ width: "200px", background: "#f0f0f0", height: "100vh", padding: "1rem" }}>
+    <aside className="bg-light p-3" style={{ width: "200px", height: "100vh" }}>
       <nav>
-        <ul style={{ listStyle: "none", padding: 0 }}>
-          <li><a href="/">Home</a></li>
-          <li><a href="/plant-guide">Plant Guide</a></li>
-          <li><a href="/encyclopedia">Encyclopedia</a></li>
+        <ul className="nav flex-column">
+          <li className="nav-item">
+            <a href="/" className="nav-link">Home</a>
+          </li>
+          <li className="nav-item">
+            <a href="/plant-guide" className="nav-link">Plant Guide</a>
+          </li>
+          <li className="nav-item">
+            <a href="/encyclopedia" className="nav-link">Encyclopedia</a>
+          </li>
         </ul>
       </nav>
     </aside>
