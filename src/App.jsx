6c6f89c29e71a5bdec51910323469028825942e7
EKO <e1304h@gmail.com>
2025-06-07 13:07:28 +0700

    fix app

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Chery_tomato from './components/home/Chery_tomato';
 import Beefsetak_tomato from './components/home/Beefsetak_tomato';
 import Roma_tomato from './components/home/Roma_tomato';
 import Heirloom_tomato from './components/home/Heirloom_tomato';
-import DashboardLayout from './components/dashboard/DashboardLayout/LayoutDashboard';
 
 function App() {
   return (
@@ -15,7 +14,6 @@ function App() {
         <Route path="/" element={<PlantGuide/>} />
         <Route path="/plant-guide" element={<PlantGuide />} />
         <Route path="/encyclopedia" element={<Encyclo />} />
-        <Route path="/dashboard" element={<DashboardLayout />} />
         <Route path="/Chery_tomato" element={<Chery_tomato />} />
         <Route path="/Beefsetak_tomato" element={<Beefsetak_tomato />} />
         <Route path="/roma_tomato" element={<Roma_tomato />} />
