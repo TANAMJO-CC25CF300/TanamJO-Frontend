fdea1a3536536a64f141f57587486778403e8ec1
EKO <e1304h@gmail.com>
2025-06-07 15:14:47 +0700

    update layout dashboard

--- src/components/dashboard/layoutdashboard.jsx
+++ src/components/dashboard/layoutdashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from './header';
+
 import Sidebar from './sidebar';
 
 const LayoutDashboard = ({ children }) => {
@@ -9,7 +9,6 @@ const LayoutDashboard = ({ children }) => {
         <Sidebar />
 
         <div className="flex-1 flex flex-col overflow-hidden">
-            <Header />
 
             <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-100 p-6">
                 {children}
