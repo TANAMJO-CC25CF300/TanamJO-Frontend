f260a292d8060ed2979c801f026cc3952c086203
EKO <e1304h@gmail.com>
2025-06-07 12:29:29 +0700

    Add DashboardLayout components

---
+++ src/components/dashboard/DashboardLayout/LayoutDashboard.jsx
@@ -0,0 +1,13 @@
+import Master from './Master';
+import Dashboard from '../Index';
+
+const LayoutDashboard = () => {
+  return (
+    <>
+      <Master />
+      <Dashboard />
+    </>
+  );
+};
+
+export default LayoutDashboard;
