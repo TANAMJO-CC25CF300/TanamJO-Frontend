f4688cbc09d2ca68e0d8ebda217d9ff78e6ce2eb
EKO <e1304h@gmail.com>
2025-06-07 11:47:30 +0700

    fix header dashboard

--- src/components/dashboard/Header-dashboard.jsx
+++ src/components/dashboard/Header-dashboard.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const HeaderDashboard = () => {
   return (
-    <header className="bg-success text-white text-center py-3">
-      <h1>Dashboard Header</h1>
-    </header>
+    <div>
+      
+    </div>  
   );
 };
 
