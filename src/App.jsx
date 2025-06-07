677f45556eda263d48a27fa0ccb475a9a9714b7d
EKO <e1304h@gmail.com>
2025-06-07 11:30:50 +0700

    Tambah app

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ import Dashboard from './components/dashboard/Index';
 import Chery_tomato from './components/home/Chery_tomato';
 import Beefsetak_tomato from './components/home/Beefsetak_tomato';
 import Roma_tomato from './components/home/Roma_tomato';
+import Heirloom_tomato from './components/home/Heirloom_tomato';
 
 function App() {
   return (
     <>
-      <Routes>
+      <Routes>  
         <Route path="/" element={<PlantGuide/>} />
         <Route path="/plant-guide" element={<PlantGuide />} />
         <Route path="/encyclopedia" element={<Encyclo />} />
