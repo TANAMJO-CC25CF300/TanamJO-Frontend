26a5d8ea6067b8dd53d40c1d3be465cef951544e
EKO <e1304h@gmail.com>
2025-06-07 09:19:33 +0700

    Tambah file Beefsetak_tomato.jsx

---
+++ src/components/home/Beefsetak_tomato.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HarvestLanding from '../plantlis/HarvestLandingHome';
+
+function Beefsetak_tomato() {
+    return (
+        <main>
+            <div>
+                <HarvestLanding title="Our History" breadcrumb={["Home", "Beefsteak Tomato"]}/>
+            </div>
+        </main>
+    )
+}
