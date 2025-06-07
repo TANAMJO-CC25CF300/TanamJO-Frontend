4b9fd66b30f50347efec321afc92ed3e4174a7cd
EKO <e1304h@gmail.com>
2025-06-07 13:39:39 +0700

    Fix import paths for Button components in Header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
-import ButtonLogin from '../Button/ButtonLogin';
-import LittenButton from '../Button/LittenButton';
+import ButtonLogin from '../button/ButtonLogin';
+import LittenButton from '../button/LittenButton';
 import logo from '../../assets/Logo/logo.svg';
 
 function Header() {
