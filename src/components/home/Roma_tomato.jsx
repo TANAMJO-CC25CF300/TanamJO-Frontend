bdebc7d8a7856efc17094cf9bfba86d9794ccd28
EKO <e1304h@gmail.com>
2025-06-07 10:14:27 +0700

    Tambah file Roma_tomato.jsx

---
+++ src/components/home/Roma_tomato.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HarvestLanding from '../plantlis/HarvestLandingHome';
+import TomatoGuide from '../plantlis/TomatoGuide';
+
+function Roma_tomato() {
+    return (
+        <main>
+            <div>
+                <HarvestLanding title="Our History" breadcrumb={["Home", "Roma Tomato"]}/>
+            </div>
+        </main>
+    )
+}
+
+export default Roma_tomato;
