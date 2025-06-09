0731d2c53ac947a369cea30a0deac0c4f4559545
EKO <e1304h@gmail.com>
2025-06-09 11:18:41 +0700

    Add artikel detail template

--- src/components/landingPages/artikel/ArtikelDetailTemplate.jsx
+++ src/components/landingPages/artikel/ArtikelDetailTemplate.jsx
@@ -18,7 +18,7 @@ function ArtikelDetailTemplate({  title, date, readTime, category, tags, descrip
                                     {title}
                                 </h2>
                                 
-                                <div className="flex items-center space-x-5 text-sm/6 mb-3.5">
+                                <div className="flex flex-wrap items-center gap-3 text-sm/6 mb-3.5">
                                     <div className="flex items-center bg-gray-50 px-3 py-1.5 rounded-full">
                                         <svg className="w-4 h-4 steel-haze mr-1.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
