3a792164abdd96601ba68fec9537d01bdf1717c0
EKO <e1304h@gmail.com>
2025-06-10 18:51:33 +0700

    menambahkan semua file

--- src/components/common/SearchInput.jsx
+++ src/components/common/SearchInput.jsx
@@ -1,44 +1,39 @@
 import React, { useState } from "react";
-import { FiSearch, FiX } from "react-icons/fi";
-import { useNavigate } from "react-router-dom";
+import { FiSearch } from "react-icons/fi";
 
-const SearchInput = () => {
+const SearchInput = ({ onClose }) => {
     const [searchTerm, setSearchTerm] = useState("");
-    const navigate = useNavigate();
 
     const handleSearch = (e) => {
         setSearchTerm(e.target.value);
     };
 
-    const handleClose = () => {
-        navigate('/');
-    };
-
     return (
-        <div className="fixed inset-0 flex items-center justify-center bg-transparent z-20">
+        <>
+        
+            <div className="fixed inset-0 bg-white/85 z-[60]" onClick={onClose}></div>
             
-            <button className="absolute top-4 sm:top-5 right-4 sm:right-8 rounded-full w-8 h-8 sm:w-10 sm:h-10 flex items-center justify-center z-30" onClick={handleClose} aria-label="Close search">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 text-gray-400">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </button>
-            
-            <div className="w-full max-w-[92%] xs:max-w-[80%] sm:max-w-xl md:max-w-2xl px-2 sm:px-6">
-                <div className="relative flex items-center border-b border-gray-400">
-                    <input type="text" placeholder="Search for plants, care tips, or solutions..." value={searchTerm} onChange={handleSearch}
-                        className="w-full bg-transparent px-4 py-3.5 text-lg tracking-wide focus:outline-none placeholder:text-gray-400 focus:placeholder:text-gray-400 transition-all duration-300 ease-in-out placeholder:font-nunito placeholder:tracking-wider placeholder:leading-normal"
-                        style={{ border: "none", boxShadow: "none", letterSpacing: "0.025em" }}
-                    />
-                    
-                    <button
-                        className="ml-2 p-2 bg-transparent hover:bg-transparent focus:outline-none"
-                        onClick={() => console.log("Searching for:", searchTerm)} aria-label="Search" tabIndex={0}
-                    >
-                        <FiSearch size={20} className="text-gray-500 " />
-                    </button>
+            <div className="fixed inset-0 flex items-center justify-center z-[70]">
+                <button className="absolute top-4 sm:top-5 right-4 sm:right-8 rounded-full w-8 h-8 sm:w-10 sm:h-10 flex items-center justify-center z-[80]" onClick={onClose} aria-label="Close search">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 text-gray-400">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
+
+                <div className="relative w-full max-w-[92%] xs:max-w-[80%] sm:max-w-xl md:max-w-2xl px-2 sm:px-6">
+                    <div className="relative flex items-center border-b border-gray-600">
+                        <input type="text" placeholder="Search for plants, care tips, or solutions..." value={searchTerm} onChange={handleSearch}
+                            className="w-full bg-transparent px-4 py-3.5 text-lg tracking-wide focus:outline-none placeholder:text-black focus:placeholder:text-gray-400 transition-all duration-300 ease-in-out placeholder:font-nunito placeholder:tracking-wider placeholder:leading-normal text-gray-900"
+                            style={{ border: "none", boxShadow: "none", letterSpacing: "0.025em" }} autoFocus
+                        />
+                        
+                        <button className="ml-2 p-2 bg-transparent hover:bg-transparent focus:outline-none" onClick={() => console.log("Searching for:", searchTerm)}  aria-label="Search" tabIndex={0}>
+                            <FiSearch size={20} className="text-gray-600" />
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- src/layout/landingPages/header.jsx
+++ src/layout/landingPages/header.jsx
@@ -1,12 +1,17 @@
 import React, { useState } from "react";
 import { Link } from 'react-router-dom';
 import logo from '@/assets/logo/logo.svg';
+import SearchInput from '@/components/common/SearchInput';
 
 function Header() {
     const [isOpen, setIsOpen] = useState(false);
+    const [isSearchOpen, setIsSearchOpen] = useState(false);
 
     const handleOpen = () => setIsOpen(true);
     const handleClose = () => setIsOpen(false);
+    
+    const handleSearchOpen = () => setIsSearchOpen(true);
+    const handleSearchClose = () => setIsSearchOpen(false);
     
     return (
 
@@ -22,11 +27,11 @@ function Header() {
                         </div>
 
                         <div className="flex xl:hidden gap-x-5">
-                            <div className="flex items-center justify-center w-10 h-10 text-white bg-white/20 backdrop-blur-sm rounded-full border-1 border-white">
+                            <button onClick={handleSearchOpen} className="flex items-center justify-center w-10 h-10 text-white bg-white/20 backdrop-blur-sm rounded-full border-1 border-white">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                                 </svg>
-                            </div>
+                            </button>
 
 
                             <button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-white" onClick={handleOpen}>
@@ -46,11 +51,11 @@ function Header() {
 
                         <div className="hidden xl:flex xl:flex-1 xl:justify-end">        
                             <div className="hidden xl:flex xl:flex-1 xl:justify-end gap-x-5">
-                                <div className="flex items-center justify-center w-11 h-11 text-white bg-white/20 backdrop-blur-sm rounded-full border-1 border-white">
+                                <button onClick={handleSearchOpen} className="flex items-center justify-center w-11 h-11 text-white bg-white/20 backdrop-blur-sm rounded-full border-1 border-white">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                                     </svg>
-                                </div>
+                                </button>
 
                                 <Link to="/login" className="text-sm/6 font-semibold text-white tracking-wide bg-white/20 backdrop-blur-sm rounded-full px-10 py-2.5 border-1 border-white">
                                     Sign Up
@@ -101,8 +106,10 @@ function Header() {
                     </div>
                 </header>
             </div>
+            
+            {/* Search Modal */}
+            {isSearchOpen && <SearchInput onClose={handleSearchClose} />}
         </>
-        
     );
 }
 
