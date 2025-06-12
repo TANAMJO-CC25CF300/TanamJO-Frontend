7075ec2a263827b8a67395db3b94cf8658eb3239
EKO <e1304h@gmail.com>
2025-06-13 01:55:27 +0700

    fix blog artikel

--- src/components/landingPages/artikel/blogArtikel.jsx
+++ src/components/landingPages/artikel/blogArtikel.jsx
@@ -24,7 +24,7 @@ function BlogArtikel() {
   return (
     <>
         <div>
-            <HarvestLandingAbout title={blogPost.title} breadcrumb={["Home", "Blog Artikel"]}/>
+            <HarvestLandingAbout title="Our History" breadcrumb={["Home", "Blog"]}/>
         </div>
 
         <div className="lg:mx-0 xl:mx-10 -mt-6 w-auto rounded-4xl text-center relative z-20 mb-5 shadow-md pattern-background">
